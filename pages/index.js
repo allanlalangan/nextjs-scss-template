bbb9d22354288f6af7040519e09c959376aa5a74
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-12 20:38:10 -0700

    init commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>nextjs-scss-template</title>
+        <meta name='description' content='Create Next App' />
+      </Head>
+      <div>
+        <h1>Create Next App</h1>
+      </div>
+    </>
+  );
+}
